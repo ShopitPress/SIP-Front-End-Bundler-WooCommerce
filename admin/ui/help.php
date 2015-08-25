36f59997459aced6cab15a04d739d200000be6ca
ShopitPress <hello@shopitpress.com>
2015-08-25 22:52:26 +0800

    help and readme

--- admin/ui/help.php
+++ admin/ui/help.php
@@ -1,3 +1,11 @@
-<?php 
-	/*Code is Here*/
-?>
+<div class="sip-tab-content">
+  <div style="display: block;">
+    <h2>Usage</h2>
+    <p>Once you create your bundles, you can display them with the shortcode [sip_front_end_bundler id="1"] (Where '1' is the ID of the bundle you want to display) in any post or page.
+    </p>
+    <h2>Questions and support</h2>
+    <p>All of our plugins come with free support. We care about your plugin after purchase just as much as you do.</p>
+    <p>We want to make your life easier and make you happy about choosing our plugins. We guarantee to respond to every inquiry within 1 business day.
+    Please visit our <a href="https://shopitpress.com/community/?utm_source=wordpress.org&amp;utm_medium=backend-help&amp;utm_campaign=sip-front-end-bundler-woocommerce" target="_blank">community</a> and ask us anything you need.</p>
+  </div>
+</div>
